3903cfa3735733778a262f6a137637cc79b72c4d
paulo <pauloh159@live.com>
2026-01-19 23:12:53 -0300

    Commit inicial - Projeto vazio

---
+++ src/api/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLiteServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
